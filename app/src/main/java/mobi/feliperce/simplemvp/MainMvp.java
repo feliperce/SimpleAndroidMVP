3cfb5588f8f8de3e97b190a3b570411418ad391b
Felipe Rodrigues <felipe.rce@gmail.com>
2017-09-26 16:50:47 -0300

    First commit

---
+++ app/src/main/java/mobi/feliperce/simplemvp/MainMvp.java
@@ -0,0 +1,20 @@
+package mobi.feliperce.simplemvp;
+
+/**
+ * Created by felipe on 26/09/17.
+ */
+
+public interface MainMvp {
+
+    interface Model {
+
+    }
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
